6f70811f41e70fe603d1149df145f4f662bc9ac8
Dhiyani.S <dhiyanisrinivasan123@gmail.com>
2026-03-01 19:07:10 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,39 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    // Adding cloud workstation host to allowed origins to fix the CORS warning
+    allowedDevOrigins: ['*.cloudworkstations.dev'],
+  },
+};
+
+export default nextConfig;
